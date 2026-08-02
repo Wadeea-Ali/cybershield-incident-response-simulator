384170c6f42ae7695e84c6ee9d4206dfdf041d0f
Wadeea-Ali <wadeeahkhamis@gmail.com>
2026-08-02 11:06:19 +0300

    Add HTML CSS and JavaScript files

---
+++ app.js
@@ -0,0 +1,4 @@
+/*Variables*/
+const startScreen=document.getElementById("tart-screen");
+const gameScreen=document.getElementById("game-screen");
+const endScreen=document.getElementById("end-screen");

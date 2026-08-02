660fd868189f65e9b238946c1e0ee67aae293380
Wadeea-Ali <wadeeahkhamis@gmail.com>
2026-08-02 12:02:15 +0300

    Update JavaScript

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 /*Variables*/
-const startScreen=document.getElementById("tart-screen");
+const startScreen=document.getElementById("start-screen");
 const gameScreen=document.getElementById("game-screen");
 const endScreen=document.getElementById("end-screen");
+const IncidentQuestion=document.querySelector( '#incident-question');
+let selectedDifficulty;
+
+const easyButton=document.querySelector('#easy-btn');
+const mediumButton=document.querySelector('#medium-btn');
+const hardButton=document.querySelector('#hard-btn');
+
+/*Event Listeners*/
+easyButton.addEventListener('click',startGame); 
+mediumButton.addEventListener('click',startGame); 
+hardButton.addEventLlistener('click',startGame);
+
+/*functions*/
+function startGame(event){
+selectedDifficulty=event.target.id;
+startScreen.classList.add('hidden');
+gameScreen.classList.remove('hidden');
+}
+
---
+++ data.js
@@ -0,0 +1,17 @@
+const easyIncidents = [
+  {
+    title: "Phishing Email",
+
+    question:
+      "You receive an email from your bank asking you to verify your account by clicking a link. What should you do?",
+
+    choices: [
+      "Click the link and log in",
+      "Forward the email to your coworkers",
+      "Report the email to the IT/Security team",
+      "Reply with your account details"
+    ],
+
+    correctChoice: 2
+  }
+];
